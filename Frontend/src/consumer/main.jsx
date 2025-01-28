695bae427239bdd363e8ab06727424998592032f
PrashannaLohani <plohani2003@gmail.com>
2025-01-28 14:47:05 +0545

    Some changes

--- Frontend/src/consumer/main.jsx
+++ Frontend/src/consumer/main.jsx
@@ -19,7 +19,7 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Suspense>
+    <Suspense fallback>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
