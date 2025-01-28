f9b17418f1330469eda33adc6bd3b67a9f230ad6
PrashannaLohani <plohani2003@gmail.com>
2025-01-28 16:45:20 +0545

    Layout chanegd and certain routing pattern changed

--- Frontend/src/consumer/main.jsx
+++ Frontend/src/consumer/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material";
+import Loader from "../Components/loader/Loader.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 const theme = createTheme({
   palette: {
@@ -19,10 +21,12 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Suspense fallback>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
