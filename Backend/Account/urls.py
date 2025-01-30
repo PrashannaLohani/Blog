0861815825419b1bd78d5baa5719543ef8e45335
PrashannaLohani <plohani2003@gmail.com>
2025-01-30 12:17:46 +0545

    Certain change in models

--- Backend/Account/urls.py
+++ Backend/Account/urls.py
@@ -3,7 +3,7 @@ from .views import *
 from .utils import FeedbackView
 
 urlpatterns = [
-    path('signup', create_user, name="create_user"),
-    path('login', login_user, name="login_user"),
-    path('feedback', FeedbackView.as_view(), name="feedback"),
+    path('signup/', create_user, name="create_user"),
+    path('login/', login_user, name="login_user"),
+    path('feedback/', FeedbackView.as_view(), name="feedback"),
 ]
