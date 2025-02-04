3f604217b2eb15bf7dc3ce10037f17dd77ae8333
PrashannaLohani <plohani2003@gmail.com>
2025-02-04 12:04:42 +0545

    Cors header setup

--- Backend/Blog_Backend/settings.py
+++ Backend/Blog_Backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'Account',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
